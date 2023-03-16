b3f463635377708cdab419e513c2274dafe50883
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-16 16:19:37 +0000

    complete layout for contact section

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,18 @@
 import {} from "./contact.styled";
-import { SectionHeading } from "../App.styled";
+import { SectionHeading, BasicText } from "../App.styled";
+import { ContactFlexContainer, ContactButton } from "./contact.styled";
 
 const Contact = () => {
   return (
     <>
       <SectionHeading>Get in touch</SectionHeading>
+      <ContactFlexContainer>
+        <BasicText align="center">
+          If you’ve got any interesting opportunities or just want to say hi my
+          inbox is always open.
+        </BasicText>
+        <ContactButton>Say Hi.</ContactButton>
+      </ContactFlexContainer>
     </>
   );
 };
--- src/components/contact/contact.styled.js
+++ src/components/contact/contact.styled.js
@@ -1,1 +1,20 @@
 import styled from "styled-components";
+
+export const ContactFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ContactButton = styled.button`
+  margin-top: 30px;
+  width: fit-content;
+  border: 3px solid #9f5dbe;
+  border-radius: 10px;
+  background-color: white;
+  padding: 7px 20px;
+  font-size: 24px;
+  font-weight: 600;
+  color: #9f5dbe;
+`;
