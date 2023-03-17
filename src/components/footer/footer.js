3bde92e5a5338048d642b69a6aaad7dbf2cd427c
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-03-17 00:27:39 +0000

    added basic footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,6 @@
+import { FooterContainer } from "./footer.styled";
+const Footer = () => {
+  return <FooterContainer>Designed & Built by Hasin Mahmood </FooterContainer>;
+};
+
+export default Footer;
---
+++ src/components/footer/footer.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+  background-color: #785598;
+  color: white;
+  font-size: 12px;
+  font-weight: 300;
+`;
